9a29c233429bbd88bc163a0d47b7114edce3bf71
Blasio Muscat <blasio.muscat@st.com>
2009-09-09 20:59:30 +0200

    Added parsing code to display read values

--- UPSMonitor/frmMonitor.vb
+++ UPSMonitor/frmMonitor.vb
@@ -1,10 +1,35 @@
 ﻿Public Class frmMonitor
 
     Private WithEvents COMPort As New Rs232
+    Private bWaiting As Boolean
 
     Private Sub tmrPoll_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrPoll.Tick
+
         If COMPort.IsOpen Then
-            COMPort.Write("Q1" & vbCr)
+            If Not bWaiting Then
+
+                COMPort.Write("Q1" & vbCr)
+                bWaiting = True
+
+            Else
+
+                Dim strBuff As String = ""
+
+                Do While InStr(strBuff, vbCr, CompareMethod.Text) = 0
+                    COMPort.Read(1)
+                    strBuff = strBuff & COMPort.InputStreamString()
+                Loop
+
+                txtVoltageIn.Text = "VIn = " & Mid(strBuff, 2, 5) & " v"
+                txtVoltageOut.Text = "VOut = " & Mid(strBuff, 14, 5) & " v"
+                txtLoadPcnt.Text = "Load = " & Mid(strBuff, 20, 3) & "%"
+                txtFreq.Text = "Freq = " & Mid(strBuff, 24, 4) & " Hz"
+                txtVoltageBatt.Text = "VBatt = " & Mid(strBuff, 29, 4) & " v"
+                txtTemp.Text = "Temp = " & Mid(strBuff, 34, 4) & " ºC"
+                txtStatus.Text = strBuff
+
+                bWaiting = False
+            End If
         End If
     End Sub
 
@@ -16,12 +41,6 @@
         ' Add any initialization after the InitializeComponent() call.
         COMPort.Open(1, 2400, 8, Rs232.DataParity.Parity_None, Rs232.DataStopBit.StopBit_1, 4096)
 
-    End Sub
-
-    Private Sub COMPort_CommEvent(ByVal Source As Rs232, ByVal Mask As Rs232.EventMasks) Handles COMPort.CommEvent
-        If (Mask Or Rs232.EventMasks.RxChar) <> 0 Then
-            MsgBox(COMPort.Read(20))
-        End If
     End Sub
 
 End Class
